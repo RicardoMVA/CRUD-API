422ae2a9d96562df713862b64dc856b3b6074f58
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-02-08 12:52:09 -0300

    use regex to do basic email validation inside DB schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 let userSchema = new mongoose.Schema({
-	name: {type: String, unique: true, required: true},
-	email: {type: String, unique: true, required: true},
-	memberSince: {type: Date, default: Date.now}
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		validate: {
+			validator: (info) => {
+				// basic email validation with regular expression suggested by MDN in
+				// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#Validation
+				return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(info);
+			},
+			message: "Invalid email"
+		},
+	},
+	memberSince: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 module.exports = mongoose.model('User', userSchema);
