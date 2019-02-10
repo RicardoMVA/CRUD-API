8448461741e44e42adb0f8c00e6f032ca7420ac4
Ricardo Alves <ricardo-valenca@hotmail.com>
2019-02-10 12:22:25 -0300

    improve error message

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const User 	  = require('../models/user');
 function checkErrorType(res, err) {
 	console.log(err);
 	if(err.code === 11000){
-		let error = 'User validation failed: email: must be unique';
+		let error = 'User validation failed: email and user must be unique';
 		res.render('error', {error: error});
 	} else {
 		let error = err.message;
